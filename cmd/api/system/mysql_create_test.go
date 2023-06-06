b5f165d53ebe7c13cc594f0d9aa2bb1f713b6e17
Roro <rodrigoignacio932@gmail.com>
2023-06-06 10:27:31 -0300

    mysqlfind wip@

--- cmd/api/system/mysql_create_test.go
+++ cmd/api/system/mysql_create_test.go
@@ -1,20 +1,8 @@
 package system_test
 
-import (
-	"context"
-	"errors"
-	"testing"
-	"time"
-
-	"github.com/DATA-DOG/go-sqlmock"
-	"github.com/stretchr/testify/assert"
-
-	"github.com/rromero96/stori/cmd/api/system"
-)
-
 const queryCreateMock string = "INSERT INTO stori.transactions \\(id, date, transaction, type\\) VALUES \\(\\?, \\?, \\?, \\?\\)"
 
-func TestMakeMySQLCreate_success(t *testing.T) {
+/* func TestMakeMySQLCreate_success(t *testing.T) {
 	db, mock, _ := sqlmock.New()
 	mock.ExpectPrepare(queryCreateMock)
 	mock.ExpectExec(queryCreateMock).WillReturnResult(sqlmock.NewResult(1, 2))
@@ -67,3 +55,4 @@ func TestMySQLCreate_failsWhenCantRunQuery(t *testing.T) {
 
 	assert.Equal(t, want, got)
 }
+*/
